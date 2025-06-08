8817fdc44a87c7e39b8cf038490f105365972b3d
Augment Chinedu <augmentchinedu@gmail.com>
2025-06-08 14:49:02 +0100

    New Changes

--- build.sh
+++ build.sh
@@ -1,16 +1,11 @@
-# Load .env file
-set -o allexport
-source ../../.env
-set +o allexport
-
 # Now use the variables
 echo "Environment: $APP_NAME"
 echo "API Key: $PORT"
 
 # Clone the parent or child repository into the current directory
-git clone https://github.com/genesiskrane/app temp-child
-
-# Rename the cloned repo folder to "app"
-mv temp-child app
+cd ../
+git clone https://github.com/genesiskrane/app app
 
 npm install  # or yarn or pnpm
+
+ls
